ee4c9aed71092f62822a9db1829b933152da94ab
nverba <distantstate@gmail.com>
2015-04-04 09:02:31 +0100

    added mock products

--- src/mock-backend.js
+++ src/mock-backend.js
@@ -10,6 +10,11 @@
     $httpBackend.when('GET', '/api/')
       .respond(JSON.stringify(HAL_JSON.root));
 
+    $httpBackend.when('GET', '/api/products/')
+      .respond(JSON.stringify(HAL_JSON.products));
+
+    // resolve template requests normally.
+
     $httpBackend.whenGET(/components/).passThrough();
 
   }
@@ -17,13 +22,13 @@
   function generateTables(hal, tables) {
 
     for (var i=1; i <= tables; i++) {
+
       hal._embedded.tables.push({
         "_links": {
-          "self": {
-              "href": "api/tables/" + i
-          }
+          "self": { "href": "api/tables/" + i }
         },
         "id": i,
+        "reserved": [],
         "_embedded": {
           "orders": []
         }
@@ -32,27 +37,55 @@
   }
 
   HAL_JSON.root = {
+
     "_links": {
-      "self": {
-        "href": "/api/"
-      }
+      "self": { "href": "/api/" },
+      "products": { "href": "/api/products/" }
     },
     "_embedded": {
       "tables": [],
       "menu": {
         "_links": {
-          "self": {
-              "href": "api/menu"
-          }
-        },
-        "id": "mwop",
-        "url": "http://www.mwop.net"
-      },
+          "self": { "href": "api/menu" }
+        }
+      }
     }
   };
 
   // push any number of tables to root
   generateTables(HAL_JSON.root, 34);
+
+  HAL_JSON.products = {
+
+    "_links": {
+      "self": { "href": "/api/products/" }
+    },
+    "_embedded": {
+      "categories": [
+        {
+          "_links": {
+            "self": { "href": "/api/products/categories/starters" },
+            "name": "Starters"
+            
+          }  
+        },
+        {
+          "_links": {
+            "self": { "href": "/api/products/categories/mains" },
+            "name": "Mains"
+            
+          }  
+        },
+        {
+          "_links": {
+            "self": { "href": "/api/products/categories/deserts" },
+            "name": "Deserts"
+            
+          }  
+        }
+      ]
+    }
+  };
 
 })();
 
