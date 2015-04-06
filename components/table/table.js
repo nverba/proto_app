96920dc46cdf22162e181455c255fe21dae9cd98
nverba <distantstate@gmail.com>
2015-04-06 11:57:18 +0100

    updated table component

--- components/table/table.js
+++ components/table/table.js
@@ -12,10 +12,25 @@
       var allocateTable = angular.bind(this, function (root) {
         this.table = root.embedded.tables[$routeParams.id -1];
         this.id = this.table.props.id;
-        this.orders = this.table.embedded.orders;
+        return root;
       });
 
-      ApiService.load().then(allocateTable);
+      var fetchOrders = angular.bind(this, function (response) {
+
+        var orders = response.embedded.table_orders.map(function (order) {
+          return order.props;
+        });
+
+        this.orders = _.filter(orders, function (order) {
+          return order.table_id == this.id;
+        }, this);
+      });
+
+      function getOpenOrders(root) {
+        root.links.table_orders.fetch({force: true}).then(fetchOrders);
+      }
+
+      ApiService.load().then(allocateTable).then(getOpenOrders);
       
     }
 
